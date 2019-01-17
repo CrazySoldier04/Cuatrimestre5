bae8e2b7512efa19d320e31e4ea47f130f71269e
Eliott Salazar <eliott01_salazar@hotmail.com>
2019-01-16 20:11:17 -0700

    .

---
+++ QualitySoftwareDevelop/Leer_Escribir_Archivo/Leer_Escribir_Archivo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leer_Escribir_Archivo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
