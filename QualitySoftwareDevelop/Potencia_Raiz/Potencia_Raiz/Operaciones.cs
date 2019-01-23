05d507b03302a81652314ef6597e75276197016f
Eliott Salazar <eliott01_salazar@hotmail.com>
2019-01-22 20:22:19 -0700

    .

---
+++ QualitySoftwareDevelop/Potencia_Raiz/Potencia_Raiz/Operaciones.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Potencia_Raiz
+{
+    class Operaciones
+    {
+        public static double Potencia(double numero, int potencia)
+        {
+            double res = 0;
+            try
+            {
+                for (int i = 1; i <= potencia; i++)
+                {
+                    res *= numero;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.ToString());
+            }
+            return res;
+        }
+    }
+}
