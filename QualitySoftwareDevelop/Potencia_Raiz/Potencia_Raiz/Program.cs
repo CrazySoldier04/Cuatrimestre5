4a76c6894344540e38b4b020bb289d34503a149a
Eliott Salazar <eliott01_salazar@hotmail.com>
2019-01-27 19:33:20 -0700

    .

--- QualitySoftwareDevelop/Potencia_Raiz/Potencia_Raiz/Program.cs
+++ QualitySoftwareDevelop/Potencia_Raiz/Potencia_Raiz/Program.cs
@@ -14,15 +14,43 @@ namespace Potencia_Raiz
             int numero1 = 0;
             int exp = 0;
             Console.Write("Ingrese un número: ");
-            numero = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Ingresa la potencia: ");
-            exp = Convert.ToInt16(Console.ReadLine());
-            Operaciones.Potencia(numero, exp);
+            bool isDouble = double.TryParse(Console.ReadLine(), out numero); //numero = Convert.ToDouble(Console.ReadLine());
+            if (!isDouble)
+            {
+                Console.WriteLine("Tipo de dato incorrecto.");
+            }
+            else
+            {
+                Console.Write("Ingresa la potencia: ");
+                bool isInt = Int32.TryParse(Console.ReadLine(), out exp); //exp = Convert.ToInt16(Console.ReadLine());
+                if (!isInt)
+                {
+                    Console.WriteLine("Tipo de dato incorrecto.");
+                }
+                else
+                {
+                    Operaciones.Potencia(numero, exp);
+                }
+            }
             Console.Write("Ingresa un número para calcular la raíz: ");
-            numero1 = Convert.ToInt16(Console.ReadLine());
-            Console.Write("Ingresa la raíz que desea calcular: ");
-            exp = Convert.ToInt16(Console.ReadLine());
-            Operaciones.Raiz(numero1, exp);
+            bool esEntero = Int32.TryParse(Console.ReadLine(), out numero1); //numero1 = Convert.ToInt16(Console.ReadLine());
+            if (!esEntero)
+            {
+                Console.WriteLine("Tipo de dato incorrecto.");
+            }
+            else
+            {
+                Console.Write("Ingresa la raíz que desea calcular: ");
+                bool isInt = Int32.TryParse(Console.ReadLine(), out exp); //exp = Convert.ToInt16(Console.ReadLine());
+                if (!isInt)
+                {
+                    Console.WriteLine("Tipo de dato incorrecto.");
+                }
+                else
+                {
+                    Operaciones.Raiz(numero1, exp);
+                }
+            }
             Console.ReadKey();
         }
     }
