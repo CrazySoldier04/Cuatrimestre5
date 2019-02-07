7154a38e9947b86243f25b9b8396d41b4a4cf3d5
Eliott Salazar <eliottsalazar@gmail.com>
2019-02-06 18:34:03 -0700

    .

--- Quiality Software Develop/Calculo_Utilidad_Trimestre/Calculo_Utilidad_Trimestre.php
+++ Quiality Software Develop/Calculo_Utilidad_Trimestre/Calculo_Utilidad_Trimestre.php
@@ -1,20 +1,52 @@
 <?php
 	//Bloque de proceso de validación.
-	if (isset($_GET["txtIngreso1"] && isset($_GET["txtIngreso2"] && isset($_GET["txtIngreso3"] && isset($_GET["txtIngreso4"] && isset($_GET["txtEgreso1"] && isset($_GET["txtEgreso2"] && isset($_GET["txtEgreso3"] && isset($_GET["txtEgreso4"])))))
+	if (isset($_GET["txtIngreso1"]))
 	{
-		//Se lleva a cabo la generación de variables y el cálculo.
 		$in1 = $_GET["txtIngreso1"];
+	}
+	if (isset($_GET["txtIngreso2"]))
+	{
 		$in2 = $_GET["txtIngreso2"];
+	}
+	if (isset($_GET["txtIngreso3"]))
+	{
 		$in3 = $_GET["txtIngreso3"];
+	}
+	if (isset($_GET["txtIngreso4"]))
+	{
 		$in4 = $_GET["txtIngreso4"];
+	}
+	if (isset($_GET["txtEgreso1"]))
+	{
 		$eg1 = $_GET["txtEgreso1"];
+	}
+	if (isset($_GET["txtEgreso2"]))
+	{
 		$eg2 = $_GET["txtEgreso2"];
+	}
+	if (isset($_GET["txtEgreso3"]))
+	{
 		$eg3 = $_GET["txtEgreso3"];
+	}
+	if (isset($_GET["txtEgreso4"]))
+	{
 		$eg4 = $_GET["txtEgreso4"];
 	}
+	if (is_numeric($in1) && is_numeric($in2) && is_numeric($in3) && is_numeric($in4) && is_numeric($eg1) && is_numeric($eg2) && is_numeric($eg3) && is_numeric($eg4))
+	{
+		$ut1 = $in1 - $eg1;
+		$ut2 = $in2 - $eg2;
+		$ut3 = $in3 - $eg3;
+		$ut4 = $in4 - $eg4;
+	}
+	//Se imprimen los resultados al usuario.
+	printf("La utilidad del primer trimestre es: " .$ut1);
+	printf("La utilidad del segundo trimestre es: " .$ut2);
+	printf("La utilidad del tercer trimestre es: " .$ut3);
+	printf("La utilidad del último trimestre es: " .$ut4);
 ?>
-/* Inicia forma donde se ingresaran los datos. */
-<form method="get">
+<!-- Formulario donde el usuario ingresará los datos -->
+<form method="POST">
 	Ingreso 1 <br>
 	<input type="text" name="txtIngreso1" placeholder="Capture el ingreso 1"> <br>
 	Ingreso 2 <br>
