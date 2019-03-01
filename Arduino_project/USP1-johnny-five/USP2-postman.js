a13e2e4bcc8ee3b948cf9fff11d5af376ff7fd5d
unknown <Eliott Salazar>
2019-02-28 19:59:01 -0700

    .

--- Arduino_project/USP1-johnny-five/USP2-postman.js
+++ Arduino_project/USP1-johnny-five/USP2-postman.js
@@ -1,20 +1,22 @@
-var express = require('express')
-var app = express()
-const axios = require('axios')
+var express = require('express');
+var app = express();
+const axios = require('axios');
+var path = require('path');
  
-app.get('./prueba.html', function (req, res)
+app.get('/prueba.html', function (req, res)
 {
-  let body = "Culo";
-  res.send(body);
+  res.sendfile(path.join('./prueba.html'));
+  //let body = document.getElementById('txtAlexis').innerHTML;
+  //res.send(body);
 })
- 
-app.listen(3000, function()
+
+/*app.listen(3000, function()
 {
     console.log('Server is running');
 });
-axios.get("http://localhost:3000/prueba.html")
+axios.get('http://localhost:3000/prueba.html')
 .then(function (respuesta)
 {
   console.log(respuesta.data);
 })
-.catch((er) => {console.log(er);});
+.catch((er) => {console.log(er);});*/
