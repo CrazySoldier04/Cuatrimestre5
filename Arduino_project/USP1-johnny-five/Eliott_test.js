c3357b6b71bc1ab451e3928ac41e5664ae89d152
unknown <Eliott Salazar>
2019-03-05 16:52:07 -0700

    .

--- Arduino_project/USP1-johnny-five/Eliott_test.js
+++ Arduino_project/USP1-johnny-five/Eliott_test.js
@@ -5,16 +5,18 @@ var app = express();
 
 function Monda()
 {
-    var x = document.getElementById('txtPrueba').value;
-    var y = document.getElementById('txtPrueba2').value;
-    alert(x + ', ' + y);
-    board.on("ready", function()
-    {
-      var lcd = new five.LCD({pins:[12,11,7,6,5,4]});
-      lcd.cursor(0,0).print(x);
-      lcd.cursor(1,0).print(y);
-    });
+    //var x = document.getElementById('txtPrueba').value;
+    //var y = document.getElementById('txtPrueba2').value;
+    var x = "Culito";
+    var y = "Chichis";
+    console.log(x + ', ' + y);
 }
+board.on("ready", function()
+{
+  var lcd = new five.LCD({pins:[12,11,7,6,5,4]});
+  lcd.cursor(0,0).print(x);
+  lcd.cursor(1,0).print(y);
+});
 
 app.get('/prueba.html', function (req, res)
 {
