ec0ae8f75dbed66111cf19f8c94f9072490ccb4a
unknown <Eliott Salazar>
2019-03-06 18:18:04 -0700

    .

--- Arduino_project/USP1-johnny-five/app.js
+++
@@ -1,9 +0,0 @@
-var five = require("johnny-five");
-var board = new five.Board();
-
-board.on("ready", function()
-{
-    var lcd = new five.LCD({pins:[12,11,7,6,5,4]});
-    lcd.cursor(0,0).print("chinguen a su");
-    lcd.cursor(1,0).print("madre todos >:V");
-});
